ad08b1a9888b0246b582bc9ce7b78a3976a99d39
Thomas de Grivel <billitch@gmail.com>
2012-11-29 04:20:13 +0100

    more secret

--- secret.lisp
+++ secret.lisp
@@ -18,33 +18,32 @@
 
 (in-package :lowh-triangle-server)
 
+;;  Secret
+
 (define-constant +secret-file+ #P"config/secret.bin" :test 'equal)
-(define-constant +secret-length+ 32)
+(define-constant +secret-length+ 256)
 (defvar *secret* (make-array +secret-length+ :element-type '(unsigned-byte 8)))
 
 (defun read-secret (file)
   (with-input-from-file (stream file :element-type '(unsigned-byte 8))
-    (read-sequence *secret* stream)
-    t))
+    (assert (= +secret-length+ (read-sequence *secret* stream)))
+    (values)))
 
 (defun create-secret ()
   (with-output-to-file (stream +secret-file+ :element-type '(unsigned-byte 8))
-    (read-secret #P"/dev/urandom")
+    (read-secret #P"/dev/random")
     (sb-posix:fchmod (sb-sys:fd-stream-fd stream) #o600)
     (write-sequence *secret* stream)
+    (force-output stream)
     (sb-posix:fchmod (sb-sys:fd-stream-fd stream) #o400)
-    t))
+    (values)))
 
 (defun load-secret ()
   (if (fad:file-exists-p +secret-file+)
       (read-secret +secret-file+)
       (create-secret)))
 
-(defun make-random-string (length)
-  (let ((seq (make-array (ceiling length 4/3) :element-type '(unsigned-byte 8))))
-    (with-input-from-file (r #P"/dev/random" :element-type '(unsigned-byte 8))
-      (read-sequence seq r))
-    (subseq (cl-base64:usb8-array-to-base64-string seq :uri t) 0 length)))
+;;  HMAC
 
 (defun hmac (&rest parts)
   (let ((h (ironclad:make-hmac *secret* :sha256)))
@@ -52,3 +51,17 @@
       (ironclad:update-hmac h (trivial-utf-8:string-to-utf-8-bytes
 			       (string part) :null-terminate t)))
     (ironclad:hmac-digest h)))
+
+;;  Random
+
+(defun make-random-bytes (length)
+  (let ((seq (make-array length :element-type '(unsigned-byte 8))))
+    (with-input-from-file (r #P"/dev/random" :element-type '(unsigned-byte 8))
+      (read-sequence seq r))
+    seq))
+
+(defun make-random-string (length)
+  (subseq (cl-base64:usb8-array-to-base64-string (make-random-bytes
+						  (ceiling length 4/3))
+						 :uri t)
+	  0 length))
