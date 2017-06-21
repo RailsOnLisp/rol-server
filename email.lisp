ec433a184964f33216c7565f4fb818b725bb50e0
Thomas de Grivel <thoxdg@gmail.com>
2017-06-21 11:42:37 +0200

    email

---
+++ email.lisp
@@ -0,0 +1,32 @@
+;;
+;;  RoL-server  -  Rails on Lisp application server
+;;
+;;  Copyright 2012-2015 Thomas de Grivel <thomas@lowh.net>
+;;
+;;  Permission to use, copy, modify, and distribute this software for any
+;;  purpose with or without fee is hereby granted, provided that the above
+;;  copyright notice and this permission notice appear in all copies.
+;;
+;;  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+;;  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+;;  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+;;  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+;;  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+;;  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+;;  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+;;
+
+(in-package :RoL-server)
+
+(defun render-email (controller action)
+  (let* ((template.txt (find-template '.txt action controller))
+	 (txt (when template.txt
+		(with-output-to-string (*template-output*)
+		  (print-template template.txt))))
+	 (template.html (find-template '.html action controller))
+	 (html (when template.html
+		 (with-output-to-string (*template-output*)
+		   (print-template template.html))))
+	 
+			 
+  
